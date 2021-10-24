20eec6e524710ecd728ec30c55259e8fa0ef0068
AnvarPro <anvarvahitov30@rambler.com>
2021-10-25 00:23:39 +0500

    Failed Test

--- src/test/java/ru/netology/PostmanEcho.java
+++ src/test/java/ru/netology/PostmanEcho.java
@@ -10,7 +10,7 @@ public class PostmanEcho {
 
         given()
                 .baseUri("https://postman-echo.com")
-                .body("First Test") // отправляемые данные (заголовки и query можно выставлять аналогично)
+                .body("Second Test") // отправляемые данные (заголовки и query можно выставлять аналогично)
 // Выполняемые действия
                 .when()
                 .post("/post")
